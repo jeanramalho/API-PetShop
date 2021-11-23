21d2e9126f769ce79c1116cb49479191ada1c0fe
Jean <jeanramalho.dev@gmail.com>
2021-11-23 16:37:48 -0300

    Implementa banco de dados sql e cria primeira tabela

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const customExpress = require('./config/customExpress.js')
+const Tabelas = require('./infra/tabelas')
+const conexao = require('./infra/conexao')
 
-const app = customExpress()
 
-app.listen(3000, () => {
-    console.log('servidor rodando na porta 3000')
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso')
+
+        Tabelas.init(conexao)
+
+        const app = customExpress()
+        
+    app.listen(3000, () => {
+        console.log('servidor rodando na porta 3000')
+    })
+
+    app.get('/' , (req,res) => {
+        res.send('Servidor rodando, tudo ok!')
+    })
+        }
 })
 
-app.get('/' , (req,res) => {
-    res.send('Servidor rodando, tudo ok!')
-})
+
 
