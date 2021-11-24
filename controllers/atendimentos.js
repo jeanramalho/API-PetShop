d1326650e12caaa370bf7523459f3712f4678a42
Jean <jeanramalho.dev@gmail.com>
2021-11-24 16:29:21 -0300

    Termina de implementar verbo post e inicia verbo get

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,7 +2,7 @@ const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
     app.get('/atendimentos' , (req,res) => {
-        res.send('você esta na rota de atendimentos, e está realizando um get')
+       Atendimento.lista(res)
     })
 
     app.post('/atendimentos' , (req,res) =>{
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -6,14 +6,53 @@ class Atendimento {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = `INSERT INTO Atendimentos SET ?`
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+        const dataEhvalida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 3
+
+        const validacoes = [{
+            nome: 'data',
+            valido: dataEhvalida,
+            mensagem: 'Data deve ser maior ou igual data atual'
+        },
+        
+        {
+            nome: 'cliente',
+            valido: clienteEhValido,
+            mensagem: 'Nome do cliente deve ter pelo menos 3 caracteres'
+        }
+    ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = `INSERT INTO Atendimentos SET ?`
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
+
+        }
+
+    }
+
+    lista(res) {
+        const sql = 'SELECT * FROM Atendimentos'
+
+        conexao.query(sql, (erro, resultados) => {
             if(erro) {
-                res.json(erro)
+                res.status(400).json(erro)
             } else {
-                res.json(resultados)
+                res.status(200).json(resultados)
             }
         })
     }
