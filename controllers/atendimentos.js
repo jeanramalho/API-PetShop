41e4c9d79689a1c9491544b92a68c8ca425afa94
Jean <jeanramalho.dev@gmail.com>
2021-11-24 17:12:34 -0300

    implementa o verbo patch do crud

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,30 @@
+const atendimentos = require('../models/atendimentos')
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
     app.get('/atendimentos' , (req,res) => {
        Atendimento.lista(res)
     })
+
+    app.get('/atendimentos/:id' , (req, res) => {
+      const id = parseInt(req.params.id)
+
+      Atendimento.buscaPorId(id, res)
+       
+    } )
+
+
+    app.patch('/atendimentos/:id', (req, res) => {
+      const id = parseInt(req.params.id)
+      const valores = req.body
+
+      Atendimento.altera(id, valores, res) 
+    })
+
+    
+
+      
+   
 
     app.post('/atendimentos' , (req,res) =>{
        const atendimento = req.body
